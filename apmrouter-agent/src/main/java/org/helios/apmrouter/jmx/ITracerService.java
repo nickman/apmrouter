e022d984f5e9ca3c2a4e02aacd5fa7c471575003
Nicholas Whitehead <nwhitehead@heliosdev.org>
2012-10-09 09:30:02 -0400

    Adding Tracer MBean.

---
+++ apmrouter-agent/src/main/java/org/helios/apmrouter/jmx/ITracerService.java
@@ -0,0 +1,46 @@
+/**
+ * Helios, OpenSource Monitoring
+ * Brought to you by the Helios Development Group
+ *
+ * Copyright 2007, Helios Development Group and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org. 
+ *
+ */
+package org.helios.apmrouter.jmx;
+
+import org.helios.apmrouter.trace.TracerImpl;
+
+/**
+ * <p>Title: ITracerService</p>
+ * <p>Description: Service to expose the tracer as a JMX MBean </p> 
+ * <p>Company: Helios Development Group LLC</p>
+ * @author Whitehead (nwhitehead AT heliosdev DOT org)
+ * <p><code>org.helios.apmrouter.jmx.ITracerService</code></p>
+ */
+public class ITracerService extends TracerImpl implements ITracerServiceMBean {
+
+	/**
+	 * Creates a new ITracerService
+	 * @param tracer The delegate tracer
+	 */
+	public ITracerService(TracerImpl tracer)  {
+		super(tracer.getHost(), tracer.getAgent(), tracer.getSubmitter());
+	}
+
+}
