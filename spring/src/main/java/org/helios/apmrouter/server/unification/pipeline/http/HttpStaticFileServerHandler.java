f009fbb75ba23c35be4e896bd67567b805a6ac4f
Nicholas Whitehead <nwhitehead@heliosdev.org>
2012-10-31 16:26:06 -0400

    DevSynch

--- spring/src/main/java/org/helios/apmrouter/server/unification/pipeline/http/HttpStaticFileServerHandler.java
+++ spring/src/main/java/org/helios/apmrouter/server/unification/pipeline/http/HttpStaticFileServerHandler.java
@@ -131,6 +131,8 @@ public class HttpStaticFileServerHandler extends AbstractHttpRequestHandler {
     
     static {
     	mimeTypesMap.addMimeTypes("application/js");
+    	mimeTypesMap.addMimeTypes("text/html");
+    	
     }
     
 
