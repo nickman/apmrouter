80fc327dcd1706b0e77ec9fc20bcdcbb7146ed18
Nicholas Whitehead <nwhitehead@heliosdev.org>
2012-12-11 09:13:06 -0500

    DevSynch

---
+++ apmrouter-agent/src/test/java/test/org/helios/apmrouter/asm/ClockClassVisitor.java
@@ -0,0 +1,61 @@
+/**
+ * Helios, OpenSource Monitoring
+ * Brought to you by the Helios Development Group
+ *
+ * Copyright 2007, Helios Development Group and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org. 
+ *
+ */
+package test.org.helios.apmrouter.asm;
+
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.MethodVisitor;
+
+/**
+ * <p>Title: ClockClassVisitor</p>
+ * <p>Description: </p> 
+ * <p>Company: Helios Development Group LLC</p>
+ * @author Whitehead (nwhitehead AT heliosdev DOT org)
+ * <p><code>test.org.helios.apmrouter.asm.ClockClassVisitor</code></p>
+ */
+
+public class ClockClassVisitor extends ClassVisitor {
+
+	/**
+	 * Creates a new ClockClassVisitor
+	 */
+	public ClockClassVisitor() {
+		super(4);
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @see org.objectweb.asm.ClassVisitor#visitMethod(int, java.lang.String, java.lang.String, java.lang.String, java.lang.String[])
+	 */
+	@Override
+	public MethodVisitor visitMethod(int access, String name, String desc,
+			String signature, String[] exceptions) {
+		// TODO Auto-generated method stub
+		return super.visitMethod(access, name, desc, signature, exceptions);
+	}
+
+
+
+}
