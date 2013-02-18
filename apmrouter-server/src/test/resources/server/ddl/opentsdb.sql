570c9da21a2dfe5cfc6a7cc21ea878f21fcf9324
Nicholas Whitehead <nwhitehead@heliosdev.org>
2013-02-18 14:32:11 -0500

    Updated for case.

--- apmrouter-server/src/test/resources/server/ddl/opentsdb.sql
+++ apmrouter-server/src/test/resources/server/ddl/opentsdb.sql
@@ -1,34 +1,35 @@
-CREATE TABLE IF NOT EXISTS metric (
-	id VARCHAR  NOT NULL,
-	name VARCHAR  NULL,
-	PRIMARY KEY (id)
+
+CREATE TABLE IF NOT EXISTS  metric (
+	"id" VARCHAR  NOT NULL,
+	"name" VARCHAR  NULL,
+	PRIMARY KEY ("id")
 	);
 
-CREATE TABLE IF NOT EXISTS tag (
-	name VARCHAR  NOT NULL,
-	value VARCHAR  NOT NULL,
-	PRIMARY KEY (name, value)
+CREATE TABLE IF NOT EXISTS  tag (
+	"name" VARCHAR  NOT NULL,
+	"value" VARCHAR  NOT NULL,
+	PRIMARY KEY ("name", "value")
 	);
 
-CREATE TABLE IF NOT EXISTS data_point (
-	id INT  NOT NULL,
-	metric_id VARCHAR  NULL,
-	timestamp TIMESTAMP  NULL,
-	long_value BIGINT  NULL,
-	double_value DOUBLE  NULL,
-	PRIMARY KEY (id),
-	CONSTRAINT data_point_metric_id_fkey FOREIGN KEY (metric_id)
-		REFERENCES metric (id) 
+CREATE TABLE IF NOT EXISTS  data_point (
+	"id" INT  NOT NULL,
+	"metric_id" VARCHAR  NULL,
+	"timestamp" TIMESTAMP  NULL,
+	"long_value" BIGINT  NULL,
+	"double_value" DOUBLE  NULL,
+	PRIMARY KEY ("id"),
+	CONSTRAINT data_point_metric_id_fkey FOREIGN KEY ("metric_id")
+		REFERENCES metric ("id") 
 	);
 
-CREATE TABLE IF NOT EXISTS metric_tag (
-	metric_id VARCHAR  NOT NULL,
-	tag_name VARCHAR  NOT NULL,
-	tag_value VARCHAR  NOT NULL,
-	PRIMARY KEY (metric_id, tag_name, tag_value),
-	CONSTRAINT metric_tag_metric_id_fkey FOREIGN KEY (metric_id)
-		REFERENCES metric (id),
-	CONSTRAINT metric_tag_tag_name_fkey FOREIGN KEY (tag_name, tag_value)
-		REFERENCES tag (name, value) 
+CREATE TABLE IF NOT EXISTS  metric_tag (
+	"metric_id" VARCHAR  NOT NULL,
+	"tag_name" VARCHAR  NOT NULL,
+	"tag_value" VARCHAR  NOT NULL,
+	PRIMARY KEY ("metric_id", "tag_name", "tag_value"),
+	CONSTRAINT metric_tag_metric_id_fkey FOREIGN KEY ("metric_id")
+		REFERENCES metric ("id"),
+	CONSTRAINT metric_tag_tag_name_fkey FOREIGN KEY ("tag_name", "tag_value")
+		REFERENCES tag ("name", "value") 
 	);
 
