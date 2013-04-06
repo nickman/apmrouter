fa94abf2d1c74f89f4d43c391b6f3ac7528caf41
nickman <nwhitehead@heliosdev.org>
2013-04-06 19:13:40 -0400

    DevSynch

---
+++ apmrouter-server/src/test/groovy/GroovyMetricURISubscriber.groovy
@@ -0,0 +1,35 @@
+import org.helios.apmrouter.trace.*;
+import org.helios.apmrouter.metric.*;
+import org.helios.apmrouter.sender.*;
+import org.helios.apmrouter.subscription.*;
+System.setProperty("org.helios.agent", "GroovyAgent");
+
+class MListener implements MetricURISubscriptionEventListener {
+    public void onNewMetric(Object newMetric) {
+        println "NEW METRIC:${newMetric.dump()}";
+    }
+    public void onMetricStateChangeEntry(Object metric) {
+        println "METRIC ENTRY:${metric.dump()}";
+    }
+    public void onMetricStateChangeExit(Object metric) {
+        println "METRIC EXIT:${metric.dump()}";
+    }
+    public void onMetricData(Object metricData) {
+        println "METRIC DATA:${metricData.dump()}";
+    }
+    public void onMetricStateChange(Object metric) {
+        println "METRIC STATE CHANGE:${metric.dump()}";
+    }
+    
+}
+listener = new MListener();
+
+
+sender = SenderFactory.getInstance().getDefaultSender();
+if(sender.subListeners.isEmpty()) {
+    sender.subscribeMetricURI("local/hserval/APMRouterServer/platform=APMRouter/component=MetricURISubscriptionService/category=RealTimeDataEvents", listener);
+    sender.registerMetricURISubscriptionEventListener(listener);
+    Thread.sleep(2000);
+}
+
+return null;
