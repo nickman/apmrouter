35484d55d9fa0fe5483e249863ddc60b9cf0cedc
nickman <nwhitehead@heliosdev.org>
2013-04-21 19:06:43 -0400

    Adding MetricURI testing GUI

---
+++ apmrouter-server/src/main/resources/www/js/metricuri.js
@@ -0,0 +1,9 @@
+var metricGridColumnModel = [
+                 			{ "sTitle": "MetricUri"},                             
+                			{ "sTitle": "Domain" },
+                			{ "sTitle": "Host" },                			
+                			{ "sTitle": "Agent" },
+                			{ "sTitle": "Namespace" },
+                			{ "sTitle": "Name" },
+                			{ "sTitle": "Type" }
+                ];
