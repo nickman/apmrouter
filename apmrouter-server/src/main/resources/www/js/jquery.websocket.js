d4133c085041a904afc17d9678908467e49e2eee
Nicholas Whitehead <nwhitehead@heliosdev.org>
2013-04-23 09:06:51 -0400

    DevSynch

--- apmrouter-server/src/main/resources/www/js/jquery.websocket.js
+++ apmrouter-server/src/main/resources/www/js/jquery.websocket.js
@@ -4,24 +4,32 @@
 
 ;(function ( $, window, document, undefined ) {
 	var settings = {};
-	function onopen() {		
-		$('#connectedLight').html(settings.connected_img);
-		console.info("WS: Connected to [%s]---[%s]", settings.wsuri, settings.connected_img);
+	function onopen() {				
+		console.info("WS: Connected to [%s]", settings.wsuri);
+		$.event.trigger('websocket-connected');
 	}
 	
 	function onmessage(message) {
-		
+		var json = JSON.parse(message.data);
+		console.info("WS: MessageEvent: [%o]:[%o]", message, json);
+		if(json.sessionid) {
+			settings.sessionid = json.sessionid;
+			$.event.trigger('websocket-sessionid', [json.sessionid]);
+		}
 	}
 	
-	function onclose(e) {
-		
+	function onclose(e) {		
+		console.info("WS: Closed");
+		$.event.trigger('websocket-disconnected');
 	}
 	
 	function onerror(e) {
 		
 	}
 
-	
+	function doConnect() {
+		
+	}
 	
     
 	$.fn.websocket = function(options) {
@@ -43,34 +51,52 @@
 		 * @param options The init options
 		 */
 		function init(wsuri, options) {		
-	    	$.fn.websocket.state = $.extend({
+	    	console.debug("Initializing jquery.websocket [%s]", wsuri);
+			$.fn.websocket.state = $.extend({
 				connectTimeout: 2000,
 				reconnectPeriod: 5000
 	    	},options||{});
-	    	
+
+			
 	    	settings = $.fn.websocket.state;
 	    	settings.wsuri = wsuri;
-			settings.connected_img = getContent("img/svg/connected-status.svg");
-			settings.disconnected_img = getContent("img/svg/disconnected-status.svg");
-			settings.connecting_img = getContent("img/svg/connecting-status.svg");	    	
+	    	settings.connectTimeoutHandle = -1;
+	    	settings.reconnectTimeoutHandle = -1;
+	    	settings.state = 'disconnected';
+
 	    	settings.ws = new WebSocket(settings.wsuri);
 	    	settings.ws.onopen = onopen;
 	    	settings.ws.onclose = onclose;
 	    	settings.ws.onerror = onerror;
-	    	settings.ws.onmessage = onmessage;
-	    	
+	    	settings.ws.onmessage = onmessage;		    		
 		}
+		
+		function isConnectingOrConnected() {
+			return settings.state == 'connecting' || settings.state == 'connected';  
+		}
+		
+		function isReconnectScheduled() {
+			return settings.reconnectTimeoutHandle != -1;
+		}
+		
+		function startReconnectLoop() {
+			if(isConnectingOrConnected() || isReconnectScheduled()) return;
+			settings.reconnectTimeoutHandle = setTimeout(function(){
+				settings.reconnectTimeoutHandle = -1;
+				$.event.trigger('websocket-connecting',[]);
+				$.apmr.connect(function(){ // callback called when reconnect times out
+					if($.apmr.isConnectingOrConnected()) return;
+					if($.apmr.config.ws) {
+						$.apmr.config.ws.close();
+					}
+					//$.apmr.config.reconnectTimeoutHandle = $.apmr.startReconnectLoop();
+				});
+			}, $.apmr.config.reconnectPauseTime);
+		},
+		
 		
 		
     }
 	
 })( jQuery, window, document );
 
-
-function getContent(url) {
-	$.get(url).then(function(data){
-		return data;
-	});
-	
-	
-}
